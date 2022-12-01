2f584018576014fe3e52b35767d995ec7c3f8011
NoemiSpurio <noemi.spurio@gmail.com>
2022-12-01 20:07:25 +0100

    bug fix

--- src/main/java/it/prova/dottori/repository/DottoreRepository.java
+++ src/main/java/it/prova/dottori/repository/DottoreRepository.java
@@ -1,12 +1,10 @@
 package it.prova.dottori.repository;
 
-import java.util.List;
-
 import org.springframework.data.repository.CrudRepository;
 
 import it.prova.dottori.model.Dottore;
 
 public interface DottoreRepository extends CrudRepository<Dottore, Long>{
 
-	public List<Dottore> findByCodiceDottore(String codiceDottore);
+	public Dottore findByCodiceDottore(String codiceDottore);
 }
