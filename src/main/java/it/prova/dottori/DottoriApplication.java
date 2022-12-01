04391554df1f2976b0aa230d6ba78459fc03198f
NoemiSpurio <noemi.spurio@gmail.com>
2022-12-01 21:27:17 +0100

    inizializzazione di due dottori

--- src/main/java/it/prova/dottori/DottoriApplication.java
+++ src/main/java/it/prova/dottori/DottoriApplication.java
@@ -1,13 +1,31 @@
 package it.prova.dottori;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class DottoriApplication {
+import it.prova.dottori.model.Dottore;
+import it.prova.dottori.service.dottore.DottoreService;
 
+@SpringBootApplication
+public class DottoriApplication implements CommandLineRunner {
+
+	@Autowired
+	private DottoreService dottoreService;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DottoriApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Dottore doc1 = new Dottore("Pluto", "Plutotto", "PLUPLU123", false, true);
+		dottoreService.inserisciNuovo(doc1);
+		
+		Dottore doc2 = new Dottore("Micetto", "Miciaccio", "MICMIC123", true, true);
+		dottoreService.inserisciNuovo(doc2);
 	}
 
 }
