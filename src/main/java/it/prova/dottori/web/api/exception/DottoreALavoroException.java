9d88ee0459165de582835601b4a012c624aa4b24
NoemiSpurio <noemi.spurio@gmail.com>
2022-12-01 21:17:41 +0100

    create due custom exceptions

---
+++ src/main/java/it/prova/dottori/web/api/exception/DottoreALavoroException.java
@@ -0,0 +1,10 @@
+package it.prova.dottori.web.api.exception;
+
+public class DottoreALavoroException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public DottoreALavoroException(String message) {
+		super(message);
+	}
+}
