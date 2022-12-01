5e5a78d8f96b1a371862885b47e611170df166be
NoemiSpurio <noemi.spurio@gmail.com>
2022-12-01 20:07:43 +0100

    creazione service di dottore

---
+++ src/main/java/it/prova/dottori/service/dottore/DottoreService.java
@@ -0,0 +1,20 @@
+package it.prova.dottori.service.dottore;
+
+import java.util.List;
+
+import it.prova.dottori.model.Dottore;
+
+public interface DottoreService {
+
+	List<Dottore> findAll();
+
+	Dottore findById(Long id);
+
+	Dottore inserisciNuovo(Dottore input);
+
+	Dottore aggiorna(Dottore input);
+
+	void rimuovi(Long id);
+
+	Dottore findByCodice(String codice);
+}
---
+++ src/main/java/it/prova/dottori/service/dottore/DottoreServiceImpl.java
@@ -0,0 +1,58 @@
+package it.prova.dottori.service.dottore;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import it.prova.dottori.model.Dottore;
+import it.prova.dottori.repository.DottoreRepository;
+import it.prova.dottori.web.api.exception.IdNotNullForInsertException;
+
+@Service
+@Transactional
+public class DottoreServiceImpl implements DottoreService {
+
+	@Autowired
+	private DottoreRepository repository;
+	
+	@Override
+	@Transactional(readOnly = true)
+	public List<Dottore> findAll() {
+		return (List<Dottore>) repository.findAll();
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public Dottore findById(Long id) {
+		return repository.findById(null).orElse(null);
+	}
+
+	@Override
+	public Dottore inserisciNuovo(Dottore input) {
+		
+		if (input.getId() != null) {
+			throw new IdNotNullForInsertException("Non puoi inserire un dottore con un id associato.");
+		}
+		
+		return repository.save(null);
+	}
+
+	@Override
+	public Dottore aggiorna(Dottore input) {		
+		return repository.save(null);
+	}
+
+	@Override
+	public void rimuovi(Long id) {
+		repository.deleteById(id);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public Dottore findByCodice(String codice) {
+		return repository.findByCodiceDottore(codice);
+	}
+
+}
