13a8e2d244d644e53f951cd02aae4d737a13cbdc
NoemiSpurio <noemi.spurio@gmail.com>
2022-12-02 10:37:33 +0100

    implementazione exception handler

---
+++ src/main/java/it/prova/dottori/web/api/exception/CustomExceptionHandler.java
@@ -0,0 +1,61 @@
+package it.prova.dottori.web.api.exception;
+
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class CustomExceptionHandler {
+
+	@ExceptionHandler(IdNotNullForInsertException.class)
+	public ResponseEntity<Object> handleIdNotNullForInsertException(IdNotNullForInsertException ex,
+			WebRequest request) {
+
+		Map<String, Object> body = new LinkedHashMap<>();
+		body.put("timestamp", LocalDateTime.now());
+		body.put("message", ex.getMessage());
+		body.put("status", HttpStatus.BAD_REQUEST);
+
+		return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(DottoreALavoroException.class)
+	public ResponseEntity<Object> handleDottoreALavoroException(DottoreALavoroException ex, WebRequest request) {
+
+		Map<String, Object> body = new LinkedHashMap<>();
+		body.put("timestamp", LocalDateTime.now());
+		body.put("message", ex.getMessage());
+		body.put("status", HttpStatus.BAD_REQUEST);
+
+		return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(DottoreNonAssegnatoAlPazienteCorrettoException.class)
+	public ResponseEntity<Object> handleDottoreNonAssegnatoAlPazienteCorrettoException(
+			DottoreNonAssegnatoAlPazienteCorrettoException ex, WebRequest request) {
+
+		Map<String, Object> body = new LinkedHashMap<>();
+		body.put("timestamp", LocalDateTime.now());
+		body.put("message", ex.getMessage());
+		body.put("status", HttpStatus.BAD_REQUEST);
+
+		return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(DottoreNotFoundException.class)
+	public ResponseEntity<Object> handleDottoreNotFoundException(DottoreNotFoundException ex, WebRequest request) {
+
+		Map<String, Object> body = new LinkedHashMap<>();
+		body.put("timestamp", LocalDateTime.now());
+		body.put("message", ex.getMessage());
+		body.put("status", HttpStatus.NOT_FOUND);
+
+		return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+	}
+}
